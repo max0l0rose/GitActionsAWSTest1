ac66aa4387c0d3ed4008f431c02e1093bc1a9dab
9956799@gmail.com <Qqq_12345>
2022-06-08 18:03:58 +0300

    11111

---
+++ src/main/java/com/example/gitactionsawstest1/GitActionsAwsTest1Application.java
@@ -0,0 +1,21 @@
+package com.example.gitactionsawstest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionsAwsTest1Application {
+
+//	void f() {
+//
+//	}
+//
+//	int f(int a) {
+//		return 0;
+//	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitActionsAwsTest1Application.class, args);
+	}
+
+}
